f67107e0402eef5d74732a6613e5104a1cc365b5
PabloLuna25 <jolunava@est.utn.ac.cr>
2026-07-03 13:31:58 -0600

    Overall app, login and register, basic locations lookup
    Overall app, login and register, basic locations lookup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin")  // ← agregar esta línea
 }
 
 android {
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.u_2026_ra_fe"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +36,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 }
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,8 +1,11 @@
 class AppRoutes {
   AppRoutes._();
 
-  static const String appTitle = 'RA FE App';
-  static const String home = '/';
+  static const String appTitle = 'PathAR';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
   static const String settings = '/settings';
   static const String navigation = '/navigation';
+  static const String search = '/search';
 }
